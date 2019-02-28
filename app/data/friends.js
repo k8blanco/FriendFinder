2d1879fafdce510f6e455f31a78bb346ace81c2c
k8blanco <katiewhite303@gmail.com>
2019-02-28 16:20:31 -0700

    populated the modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,37 +2,37 @@
 
 var friendsArray = [{
         name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Ahmed_Berhan_2014.jpg/250px-Ahmed_Berhan_2014.jpg",
         scores: [4, 1, 3, 2, 4, 1, 2, 4, 1, 1]
     },
     {
         name: "Pixel",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo: "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/1009910_10100770807557528_1424829774_n.jpg?_nc_cat=103&_nc_ht=scontent.fapa1-1.fna&oh=c9a322a0d6bf42809177f1c2ba12a872&oe=5CE5E306",
         scores: [1, 1, 2, 3, 1, 1, 2, 2, 1]
     },
     {
         name: "George",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/George_Clooney_2016.jpg/220px-George_Clooney_2016.jpg",
         scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     },
     {
         name: "Katie",
-        photo: "",
+        photo: "https://scontent.fapa1-1.fna.fbcdn.net/v/t31.0-8/26678646_10103655293519538_883797436808100386_o.jpg?_nc_cat=110&_nc_ht=scontent.fapa1-1.fna&oh=c888c60e1237d1da2111479c74da8425&oe=5CE42652",
         scores: [4, 2, 2, 4, 1, 1, 3, 2, 3, 4]
     },
     {
         name: "Jeff",
-        photo: "",
+        photo: "https://scontent.fapa1-2.fna.fbcdn.net/v/t31.0-8/21586824_10103417811460488_5825786592249175221_o.jpg?_nc_cat=100&_nc_ht=scontent.fapa1-2.fna&oh=42ab8f9a34c6906bd51aea1d21a561b3&oe=5CDADCD6",
         scores: [1, 1, 2, 2, 1, 3, 4, 3, 2, 4]
     },
     {
         name: "Kokomo",
-        photo: "",
+        photo: "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/13124536_10102330143376048_5751403250137842843_n.jpg?_nc_cat=110&_nc_ht=scontent.fapa1-1.fna&oh=66dfe8973bdcb4d4d2ae1791966bd76d&oe=5D2228BA",
         scores: [2, 3, 3, 4, 3, 4, 1, 3, 2, 1]
     },
     {
         name: "Jillian",
-        photo: "",
+        photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/766/7-genius-weight-loss-tips-from-jillian-michaels-1505500562.jpg?resize=480:*",
         scores: [3, 4, 4, 1, 2, 2, 1, 4, 4, 3]
     }
 ]
